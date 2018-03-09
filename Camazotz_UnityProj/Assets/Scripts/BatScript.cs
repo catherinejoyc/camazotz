99fae27ce71413da4075a5816c89517de4691fdf
Catherine Calupas <cal17425@spengergasse.at>
2018-03-09 10:21:58 +0100

    Fledermaus Sterbeverhalten hinzugefügt

---
+++ Camazotz_UnityProj/Assets/Scripts/BatScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatScript : MonoBehaviour {
+
+    bool active;
+    Vector3 currPos;
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(active)
+        {
+        }
+        else
+        {
+            currPos = this.transform.position;
+
+        }
+	}
+
+    public void Die()
+    {
+        rb.useGravity = true;
+    }
+}
---
+++ Camazotz_UnityProj/Assets/Scripts/Frequency.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frequency : MonoBehaviour {
+
+    float cooldStart = -3;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bat"))
+            other.gameObject.GetComponent<BatScript>().Die();
+    }
+
+    private void Awake()
+    {
+        GetComponent<Collider>().enabled = false;
+    }
+
+    private void Update()
+    {
+        //Frequency Attack
+        if (Input.GetButton("Fire2"))
+        {
+            if (cooldStart + 3f <= Time.time)
+            {
+                GetComponent<Collider>().enabled = true;
+                cooldStart = Time.time;
+            }
+        }
+        else
+            GetComponent<Collider>().enabled = false;
+    }
+}
